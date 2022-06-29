e1a6e23dd817a4607d44e761d0a3903bf612de82
Guilherme Figueiredo <guilherme_figueiredo@outlook.com.br>
2022-06-29 14:51:22 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome = "guilherme"
+let numero = 312
+let anoAtual = 2022
+let resultado = numero + anoAtual
+
+console.log(resultado)
+
+console.log("estou mexendo no console.log")
+
+let quartaFeira = true
+
+console.log(typeof quartaFeira)
